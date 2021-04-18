c8cd93fac7efdc68c97965c421c0ef4b5ed21c66
Islands5 <five.islands.04010613@gmail.com>
2021-04-18 20:32:27 +0900

    add material-ui

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,11 +1,16 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 
+import { Button } from '@material-ui/core';
+
 export const TopPage: FC = () => {
   return (
     <React.Fragment>
       <div>TopPage</div>
       <Link to="/users">ユーザー一覧</Link>
+      <div>
+        <Button variant="contained" color="primary">Hello World</Button>
+      </div>
     </React.Fragment>
   )
 }
