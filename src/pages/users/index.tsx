eb96150c920b16394ea229f75bff749ea8598f20
Islands5 <five.islands.04010613@gmail.com>
2021-04-30 10:28:45 +0900

    useStateとuseEffectの利用例を追加

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const UserIndexPage: FC = () => {
+
+  // START: useEffect のサンプル
+  // コンポーネントを起動したとき一度だけ実行するには空の配列を渡す
+  useEffect(() => {
+    // apiの取得とかを行うといい
+    console.log('読み込み時一度呼ばれる')
+  }, [])
+
+  // 値が変更されたタイミングで再度実行される(ReactDevToolでStateをいじったりするとテストできます)
+  const [example, setExample] = useState({value: 0})
+  useEffect(() => {
+    console.log('値の変更ごとに呼ばれる')
+  }, [example])
+
+  // コンポーネントがなくなるタイミングで実行される
+  useEffect(() => {
+    return () => {
+      console.log('なくなるタイミングで一度実行される')
+    }
+  }, [])
+  // END: useEffect のサンプル
+
   return (
     <React.Fragment>
       <div>UserIndex</div>
---
+++ src/shared/http_client/index.tsx
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+// 取得するライブラリは変わる可能性があるためラップしてる
+export const HttpClient = () => {
+  return axios
+}
